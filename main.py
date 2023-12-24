9674c1c8781f60e8234f0083dd871124d2e5ddb1
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2023-12-25 05:04:22 +0600

    fix: final bot with image

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def escape_markdown(text):
 
 
 # Async function to handle API requests
-async def handle_api_request(message, delete=False):
+async def handle_api_request(message, delete=False, image_url=None):
     user_id = str(message.from_user.id)
     message_id = str(message.message_id)
 
@@ -42,12 +42,17 @@ async def handle_api_request(message, delete=False):
             async with session.delete(f"{BASE_API_URL}/delete/{user_id}", headers=headers) as response:
                 yield "Conversation deleted." if response.status == 200 else "Error in deletion."
         else:
-            # Send POST request for handling streaming response
+            # Determine if the message is a text or photo with caption
+            query_text = message.caption if message.content_type == 'photo' else message.text
+
             data = {
                 "message_id": message_id,
-                "query": message.text,
-                "conversation_id": user_id
+                "query": query_text,
             }
+            if image_url:  # Add image information if available
+                data["image"] = True
+                data["image_url"] = image_url
+
             async with session.post(f"{BASE_API_URL}/conversations/{user_id}", headers=headers, json=data) as response:
                 if response.status == 200:
                     async for line in response.content.iter_any():
@@ -59,16 +64,22 @@ async def handle_api_request(message, delete=False):
 
 
 # Function to handle incoming messages
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(content_types=['text', 'photo'])
 def handle_text(message):
     # Detect if the message is a delete message command
-    delete_command = message.text.strip() in ['/delete', '/clear']
+    delete_command = message.text.strip() in ['/delete', '/clear'] if message.text else False
+
+    # Determine if the message contains an image and/or caption
+    image_url = None
+    if message.content_type == 'photo':
+        # Get file information of the largest size image
+        file_info = bot.get_file(message.photo[-1].file_id)
+        image_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_info.file_path}"
 
     # Asynchronously process the response
     async def process_response():
         last_chunk_received = False
-        async for response in handle_api_request(message, delete=delete_command):
-            # Escape Markdown characters
+        async for response in handle_api_request(message, delete=delete_command, image_url=image_url):
             formatted_response = escape_markdown(response)
             bot.send_message(message.chat.id, formatted_response, parse_mode='MarkdownV2')
             if not last_chunk_received:
